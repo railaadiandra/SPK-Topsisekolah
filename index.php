7d7b3172fb243f797c004589306b29e6963ef381
Railaadiandra <Railaadiandra@gmail.com>
2023-01-25 08:12:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+//koneksi
+session_start();
+include "koneksi.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>SPK TOPSIS</title>
+  <!--bootstrap-->
+  <link href="tampilan/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <style>
+    body {
+      background-image: url(img/bro.jpg);
+
+    }
+  </style>
+</head>
+
+<body>
+
+  <!--menu-->
+  <nav class="navbar navbar navbar-default navbar-custom">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Sistem Pendukung Keputusan Pemilihan Pembangunan Sekolah (Metode Topsis)</a>
+      </div>
+
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="kriteria.php">Kriteria</a>
+          </li>
+          <li>
+            <a href="alternatif.php">Alternatif</a>
+          </li>
+          <li>
+            <a href="poin.php">Nilai</a>
+          </li>
+          <li>
+            <a href="nilmat.php">Nilai Matriks</a>
+          </li>
+          <li>
+            <a href="hastop.php">Hasil Topsis</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="panel-body">
+    <!--form pengisian-->
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading text-center">
+
+            <h4>SISTEM PENDUKUNG KEPUTUSAN</h4>
+            <h5>Tempat Pembangunan Sekolah Terbaik</h5>
+            <h5>Menggunakan Metode Topsis</h5><br>
+            Aplikasi ini Bermanfaat untuk menjadi acuan <br>
+            sistem Pengambilan Keputusan dalam memilih untuk <br>
+            Pembangunan Lokasi Sekolah Terbaik<br><br>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--footer-->
+    <footer class="text-center">
+      <div class="footer-below">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <em>SPK &copy; Kelompok 4</em>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!--plugin-->
+    <script src="tampilan/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
